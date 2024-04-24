2635403bce8a3d209a66e6cfdd73c4e22c635e1c
Jonathan X <115114569+jxavioer@users.noreply.github.com>
2024-04-23 22:52:22 -0400

    Shopping Cart Displayed on Right

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -19,102 +19,107 @@ function Menu() {
     };
 
     return (
-      <Container>
-          <Tabs variant="underline"
-              defaultActiveKey="appetizers"
-              id="menu-categories"
-              className="mb-3">
-              <Tab eventKey="appetizers" title="Appetizers">
-                  <Card style={{width: '16rem', border: "2px solid black", display: "inline-block"}}>
-                      <Card.Img src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Edamame</Card.Title>
-                                  <Card.Subtitle>$100</Card.Subtitle>
-                                  <Card.Text>Edamame</Card.Text>
-                              </Container>
-                              <Button className="pink-button" onClick={() => addToCart({ name: "Edamame", price: 100 })}>Add to Cart</Button>
-                          </div>
-                      </CardBody>
-                  </Card>
-                  <Card style={{width: '16rem', border: "2px solid black", display: "inline-block"}}>
-                      <Card.Img src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Edamame 2</Card.Title>
-                                  <Card.Subtitle>$101</Card.Subtitle>
-                                  <Card.Text>Edamame 2</Card.Text>
-                              </Container>
-                              <Button className="pink-button" onClick={() => addToCart({ name: "Edamame 2", price: 101 })}>Add to Cart</Button>                          </div>
-                      </CardBody>
-                  </Card>
-              </Tab>
-              <Tab eventKey="entrees" title="Entrees">
-                  <Card style={{width: '16rem', border: "2px solid black"}}>
-                      <Card.Img src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Steak Hibachi</Card.Title>
-                                  <Card.Subtitle>$100</Card.Subtitle>
-                                  <Card.Text>Steak Hibachi</Card.Text>
-                              </Container>
-                              <Button className="pink-button"
-                                      onClick={() => addToCart({name: "Steak Hibachi", price: 100})}>Add to
-                                  Cart</Button>
-                          </div>
-                      </CardBody>
-                  </Card>
-              </Tab>
-              <Tab eventKey="sushi" title="Sushi">
-                  <Card style={{width: '16rem', border: "2px solid black"}}>
-                      <Card.Img variant="Top" src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Rick Roll</Card.Title>
-                                  <Card.Subtitle>$100</Card.Subtitle>
-                                  <Card.Text>Rick Roll</Card.Text>
-                              </Container>
-                              <Button className="pink-button" onClick={() => addToCart({ name: "Rick Roll", price: 100 })}>Add to Cart</Button>
-                          </div>
-                      </CardBody>
-                  </Card>
-              </Tab>
-              <Tab eventKey="beverages" title="Beverages">
-                  <Card style={{width: '16rem', border: "2px solid black"}}>
-                      <Card.Img src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Boba</Card.Title>
-                                  <Card.Subtitle>$100</Card.Subtitle>
-                                  <Card.Text>Boba</Card.Text>
-                              </Container>
-                              <Button className="pink-button" onClick={() => addToCart({ name: "Boba", price: 100 })}>Add to Cart</Button>
-                          </div>
-                      </CardBody>
-                  </Card>
-              </Tab>
-              <Tab eventKey="desserts" title="Desserts">
-                  <Card style={{width: '16rem', border: "2px solid black"}}>
-                      <Card.Img src={Edamame}/>
-                      <CardBody>
-                          <div style={{display: "flex"}}>
-                              <Container>
-                                  <Card.Title>Mochi</Card.Title>
-                                  <Card.Subtitle>$100</Card.Subtitle>
-                                  <Card.Text>Mochi</Card.Text>
-                              </Container>
-                              <Button className="pink-button" onClick={() => addToCart({ name: "Mochi", price: 100 })}>Add to Cart</Button>
-                          </div>
-                      </CardBody>
-                  </Card>
-              </Tab>
-          </Tabs>
-          <ShoppingCartList cartItems={cartItems}/>
+      <Container style={{display: "flex"}}>
+          <Container>
+              <Tabs variant="underline"
+                  defaultActiveKey="appetizers"
+                  id="menu-categories"
+                  className="mb-3">
+                  <Tab eventKey="appetizers" title="Appetizers">
+                      <Card style={{width: '16rem', border: "2px solid black", display: "inline-block"}}>
+                          <Card.Img src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Edamame</Card.Title>
+                                      <Card.Subtitle>$100</Card.Subtitle>
+                                      <Card.Text>Edamame</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button" onClick={() => addToCart({ name: "Edamame", price: 100 })}>Add to Cart</Button>
+                              </div>
+                          </CardBody>
+                      </Card>
+                      <Card style={{width: '16rem', border: "2px solid black", display: "inline-block"}}>
+                          <Card.Img src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Edamame 2</Card.Title>
+                                      <Card.Subtitle>$101</Card.Subtitle>
+                                      <Card.Text>Edamame 2</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button" onClick={() => addToCart({ name: "Edamame 2", price: 101 })}>Add to Cart</Button>                          </div>
+                          </CardBody>
+                      </Card>
+                  </Tab>
+                  <Tab eventKey="entrees" title="Entrees">
+                      <Card style={{width: '16rem', border: "2px solid black"}}>
+                          <Card.Img src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Steak Hibachi</Card.Title>
+                                      <Card.Subtitle>$100</Card.Subtitle>
+                                      <Card.Text>Steak Hibachi</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button"
+                                          onClick={() => addToCart({name: "Steak Hibachi", price: 100})}>Add to
+                                      Cart</Button>
+                              </div>
+                          </CardBody>
+                      </Card>
+                  </Tab>
+                  <Tab eventKey="sushi" title="Sushi">
+                      <Card style={{width: '16rem', border: "2px solid black"}}>
+                          <Card.Img variant="Top" src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Rick Roll</Card.Title>
+                                      <Card.Subtitle>$100</Card.Subtitle>
+                                      <Card.Text>Rick Roll</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button" onClick={() => addToCart({ name: "Rick Roll", price: 100 })}>Add to Cart</Button>
+                              </div>
+                          </CardBody>
+                      </Card>
+                  </Tab>
+                  <Tab eventKey="beverages" title="Beverages">
+                      <Card style={{width: '16rem', border: "2px solid black"}}>
+                          <Card.Img src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Boba</Card.Title>
+                                      <Card.Subtitle>$100</Card.Subtitle>
+                                      <Card.Text>Boba</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button" onClick={() => addToCart({ name: "Boba", price: 100 })}>Add to Cart</Button>
+                              </div>
+                          </CardBody>
+                      </Card>
+                  </Tab>
+                  <Tab eventKey="desserts" title="Desserts">
+                      <Card style={{width: '16rem', border: "2px solid black"}}>
+                          <Card.Img src={Edamame}/>
+                          <CardBody>
+                              <div style={{display: "flex"}}>
+                                  <Container>
+                                      <Card.Title>Mochi</Card.Title>
+                                      <Card.Subtitle>$100</Card.Subtitle>
+                                      <Card.Text>Mochi</Card.Text>
+                                  </Container>
+                                  <Button className="pink-button" onClick={() => addToCart({ name: "Mochi", price: 100 })}>Add to Cart</Button>
+                              </div>
+                          </CardBody>
+                      </Card>
+                  </Tab>
+              </Tabs>
+          </Container>
+          <Container>
+              <h2>Shopping Cart</h2>
+            <ShoppingCartList cartItems={cartItems}/>
+          </Container>
       </Container>
     );
 }
